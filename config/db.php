bd5446b580a74c9e0d348ad3a1b1b26007649a2e
ksetrin <ksetrinn@gmail.com>
2017-11-28 01:48:39 +0500

    prod config

--- config/db.php
+++ config/db.php
@@ -1,14 +1,32 @@
 <?php
-
+/*
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=kse2modules',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
+*/
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_b2bb57019e321a9',
+    'username' => 'b52c293abcf6c3',
+    'password' => '8deaf7c4',
+    'charset' => 'utf8',
+];
+
+
+//'dsn' => 'pgsql:host=localhost;port=5432;dbname=mydatabase', // PostgreSQL
+
+/*
+C:\xampp\htdocs\kse2modules>heroku addons:add cleardb:ignite
+Adding cleardb:ignite on ksetrin... done, v26 (free)
+Use `heroku addons:docs cleardb` to view documentation.
+
+C:\xampp\htdocs\kse2modules>heroku config
+=== ksetrin Config Vars
+CLEARDB_DATABASE_URL: mysql://b52c293abcf6c3:8deaf7c4@us-cdbr-iron-east-02.clear
+db.net/heroku_b2bb57019e321a9?reconnect=true
+*/
