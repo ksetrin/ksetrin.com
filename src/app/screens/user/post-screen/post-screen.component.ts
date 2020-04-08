cbbca3b789ef1de834fdac2a4c11b23bcb56c374
ksetrin <ksetrinn@gmail.com>
2020-04-08 19:43:04 +0300

    init main layout and screens

---
+++ src/app/screens/user/post-screen/post-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-screen',
+  templateUrl: './post-screen.component.html',
+  styleUrls: ['./post-screen.component.scss']
+})
+export class PostScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
