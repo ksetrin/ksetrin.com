80749628e13a02cc9ad2e6e507820e391428f440
ksetrin <ksetrinn@gmail.com>
2020-04-10 02:09:52 +0300

    todo list: done and remove

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import uuidv4 from 'uuid/v4'
+import {ToDo} from '../../types/todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -8,16 +10,41 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class TodoListComponent implements OnInit {
 
-  todoList = [];
+  todoList: ToDo[] = [
+    {id: uuidv4(), value: 'test undone', done: false},
+    {id: uuidv4(), value: 'test done', done: true},
+  ];
   todoItem = new FormControl('', [Validators.required]);
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
   add() {
-    this.todoList.push(this.todoItem.value);
+    this.todoList.push({
+      id: uuidv4(),
+      value: this.todoItem.value,
+      done: false
+    });
     this.todoItem.reset();
   }
+
+  remove(id: string) {
+    this.todoList = this.todoList.filter((t: ToDo) => t.id !== id);
+  }
+
+  doneToggle(id: string) {
+    this.todoList = this.todoList.map((t: ToDo) => (
+      t.id === id
+        ? {
+          ...t,
+          done: !t.done
+        }
+        : t
+      )
+    );
+  }
+
 }
---
+++ src/app/types/todo.ts
@@ -0,0 +1,5 @@
+export interface ToDo {
+  id: string;
+  value: string;
+  done: boolean;
+}
